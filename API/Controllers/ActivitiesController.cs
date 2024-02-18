99fcd5c1facd38685d6120aa4caf9dd016471060
Gustavo Borges <Gustavo.Borges.M@outlook.com>
2024-02-18 20:30:10 -0300

    Created first migration and started the controller

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class ActivitiesController : BaseApiController
+    {
+        public ActivitiesController()
+        {
+                
+        }
+    }
+}
--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -9,6 +9,14 @@ namespace Persistence
         {
         }
 
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.ApplyConfigurationsFromAssembly(typeof(DataContext).Assembly);
+        //    Seed.SeedData(modelBuilder);
+            
+        //    base.OnModelCreating(modelBuilder);
+        //}
+
         public DbSet<Activity> Activities { get; set; }
     }
 }
